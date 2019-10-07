e9c13feb40759dd58c3d7c0cf7493eb0a84257b4
Olya Severova <34347298+olyaseverova@users.noreply.github.com>
2019-10-07 18:57:22 +0300

    Исправленная ошибка

--- js/main.js
+++ js/main.js
@@ -115,7 +115,9 @@ var roomsElement = document.querySelector('#room_number');
 var capacityElement = document.querySelector('#capacity');
 
 var changeRoomsOrCapacity = function () {
-  if ((Number.parseInt(roomsElement.value, 10) <= 3 && capacityElement.value > roomsElement.value) || (Number.parseInt(roomsElement.value, 10) === 100 && capacityElement.value !== '0')) {
+  if (
+    (Number.parseInt(roomsElement.value, 10) <= 3 && Number.parseInt(roomsElement.value, 10) > 0 && (capacityElement.value > roomsElement.value || capacityElement.value === '0'))
+    || (roomsElement.value === '100' && capacityElement.value !== '0')) {
     capacityElement.setCustomValidity('error');
     roomsElement.setCustomValidity('error');
   } else {

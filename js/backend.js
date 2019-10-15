dab80b3541b967142a546c251c90605523efbaad
Olya Severova <34347298+olyaseverova@users.noreply.github.com>
2019-10-15 15:16:35 +0300

    Работа с сетью

---
+++ js/backend.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+
+  var load = function (onLoad, onError) {
+    var URL = 'https://js.dump.academy/keksobooking/data';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+
+  window.backend = {
+    load: load
+  };
+
+})();

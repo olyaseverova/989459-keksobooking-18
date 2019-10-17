402ee181f03904e71be11a95f4624d1f31f42e61
Olya Severova <34347298+olyaseverova@users.noreply.github.com>
2019-10-17 18:05:04 +0300

    Структуры данных

--- js/data.js
+++ js/data.js
@@ -2,26 +2,40 @@
 
 (function () {
 
+  var PINS_QUANTITY = 5;
+
   var pinsElement = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
   var mainPinElement = document.querySelector('#pin');
+  var typeElement = document.querySelector('#type');
 
   window.data = {
     ads: null,
   };
 
   var successHandler = function (data) {
-    window.data.ads = data;
+    var isFirstTime = false;
+    if (!window.data.ads) {
+      isFirstTime = true;
+      window.data.ads = data;
+    }
 
+    var k = 0;
     for (var i = 0; i < window.data.ads.length; i++) {
-      var clonePin = mainPinElement.content.cloneNode(true);
-      var clonePinElement = clonePin.querySelector('.map__pin');
-      var pinImageElement = clonePinElement.querySelector('img');
-      clonePinElement.style.left = window.data.ads[i].location.x + 'px';
-      clonePinElement.style.top = window.data.ads[i].location.y + 'px';
-      pinImageElement.src = window.data.ads[i].author.avatar;
-      pinImageElement.alt = window.data.ads[i].offer.title;
-      fragment.appendChild(clonePinElement);
+      if (k >= PINS_QUANTITY) {
+        break;
+      }
+      if (isFirstTime || typeElement.value === window.data.ads[i].offer.type) {
+        k++;
+        var clonePin = mainPinElement.content.cloneNode(true);
+        var clonePinElement = clonePin.querySelector('.map__pin');
+        var pinImageElement = clonePinElement.querySelector('img');
+        clonePinElement.style.left = window.data.ads[i].location.x + 'px';
+        clonePinElement.style.top = window.data.ads[i].location.y + 'px';
+        pinImageElement.src = window.data.ads[i].author.avatar;
+        pinImageElement.alt = window.data.ads[i].offer.title;
+        fragment.appendChild(clonePinElement);
+      }
     }
 
     window.map = {
@@ -29,6 +43,12 @@
       fragment: fragment
     };
   };
+
+  typeElement.addEventListener('change', function () {
+    successHandler();
+    window.map.pinsElement.innerHTML = '';
+    window.map.pinsElement.appendChild(window.map.fragment);
+  });
 
   var errorHandler = function () {
     var errorElement = document.querySelector('#error');
